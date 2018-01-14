627517ebab43969bfdd8d73acb454af58d3fd147
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 00:49:35 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -8,6 +8,16 @@ console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some porti
 var livePerformance = (500+750+3000+5100+12980+9087+50000)
 var musicSales = (500+750+3000+5100+12980+9087+50000)
 
+
+function addNumbers (num1, num2, num3, num4) {
+  return num1 + num2 + num3 + num4
+}
+
+addNumbers (500,750,3000,5100,12980,9087,50000)
+
+/*I think this is where I now need to learn looping? To have just one num 
+
+
 console.log(livePerformance)
 console.log(musicSales)
 
