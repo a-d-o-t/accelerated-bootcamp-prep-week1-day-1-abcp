119f3d2dfec3f3d61f07305f2b6d9418e98ffa36
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 01:32:38 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -6,9 +6,11 @@ var musicSales = (500+750+3000+5100+12980+9087+50000)
 
 function beyonceEarnings(num1, num2, num3, num4, num5, num6, num7) {
   return num1 + num2 + num3 + num4 + num5 + num6 + num7
-}
 
 var totalSales = beyonceEarnings (500,750,3000,5100,12980,9087,50000)
+
+function bTotal(totalSales)
+}
 
 /*I think this is where I now need to learn looping? To have just one num argument?*/
 
